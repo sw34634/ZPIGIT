0ab04288726707d650937b0dfedf0a7bba80a944
sw34634 <34634@studentwsei.pl>
2020-03-20 19:41:40 +0100

    change main.cpp 2

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Hello, Git!" << std::endl;
     std::cout << "Hello, Git!" << std::endl;
     std::cout << "Hello, Git!" << std::endl;
+    std::cout << "Hello, Git!" << std::endl;
     return 0;
 }
