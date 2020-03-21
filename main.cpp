c5db9b7b103f6acbdbda4219a30f18703d9e47f3
sw34634 <34634@studentwsei.pl>
2020-03-21 13:35:48 +0100

    change main.cpp 5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello" << std::endl;
-    std::cout << "Hello" << std::endl;
-    std::cout << "Hello" << std::endl;
-    std::cout << "Hello" << std::endl;
+    std::cout << "Hello 1" << std::endl;
+    std::cout << "Hello 2" << std::endl;
+    std::cout << "Hello 3" << std::endl;
+    std::cout << "Hello 4" << std::endl;
     return 0;
 }
